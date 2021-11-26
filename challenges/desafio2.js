cba572abbcf78428ef04fcbaa66a06f3e1b58614
Ivan dos Santos Silva <ivan@pop-os.localdomain>
2021-11-26 19:46:46 -0300

    ajuste desafio 2

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,12 +1,20 @@
 db.movies.aggregate([
   {
-    $group: {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  {
+    $project: {
       _id: {
-        "titulo": "$title",
-        "avaliado": "$rated", 
-        "notaIMDB": "$imdb.rating",
-        "votosIMDB": "$imdb.votes",
-        "ano": "$year",
+        titulo: "$title",
+        avaliado: "$rated",
+        notaIMDB: "$imdb.rating",
+        votosIMDB: "$imdb.votes",
+        ano: "$year",
       },
     },
   },
