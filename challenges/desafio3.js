65f033707026947f69462b0f451c291824f4621e
Ivan dos Santos Silva <ivan@pop-os.localdomain>
2021-11-26 21:15:12 -0300

    desafio 3

---
+++ challenges/desafio3.js
@@ -0,0 +1,35 @@
+db.movies.aggregate([
+  {
+    $match: {
+      "imdb.rating": { $gte: 7 },
+      genres: { $nin: ["Crime", "Horror"] },
+      rated: { $in: ["PG", "G"] },
+      languages: { $all: ["English", "Spanish"] },
+    },
+  },
+  {
+    $project: {
+      _id: false,
+      titulo: "$title",
+      avaliado: "$rated",
+      notaIMDB: "$imdb.rating",
+      votosIMDB: "$imdb.votes",
+      ano: "$year",
+    },
+  },
+  {
+    $sort: {
+      titulo: 1,
+    },
+  },
+  {
+    $sort: {
+      ano: -1,
+    },
+  },
+  {
+    $sort: {
+      notaIMDB: -1,
+    },
+  },
+]);
