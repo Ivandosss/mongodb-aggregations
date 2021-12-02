d500e29f858fca1f9917da903d7f492ad7b9c1f6
Ivan dos Santos Silva <ivan@pop-os.localdomain>
2021-12-01 23:51:02 -0300

    desafio 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,23 @@
+db.movies.aggregate([
+  {
+    $match: {
+      languages: { $all: ["English"] } },
+  },
+  {
+    $unwind: "$cast",
+  },
+  {
+    $group: {
+      _id: "$cast",
+      numeroFilmes: { $sum: 1 },
+      media: { $avg: "$imdb.rating" },
+    },
+  },
+  {
+    $project: {
+      mediaImdb: { $round: ["$media", 1] },
+      numeroFilmes: 1,
+    },
+  },
+  { $sort: { numeroFilmes: -1, _id: -1 } },
+]);
